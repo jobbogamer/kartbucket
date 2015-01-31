db05bf40ce33c19322cdf8cdaa51de3609a9ea87
Josh Asch <josh.asch@gmail.com>
2015-01-31 21:55:21 +0000

    Hide irrelevant fields in the edit popup

--- static/kartbucket.js
+++ static/kartbucket.js
@@ -1,5 +1,12 @@
 
-function prepareEditModal(trackID, trackName, personID, personName, character, kart, wheels, glider)
+var trackID = -1;
+var personID = -1;
+var character = "";
+var kart = "";
+var wheels = "";
+var glider = "";
+
+function prepareEditModal(trackID, trackName, personID, personName, character, kart, wheels, glider, choiceOfKart, allowsCustomisation)
 {
     $("#edit-modal .modal-title").html("Add Time - " + trackName);
     $("#edit-modal-person-field").val(personID);
@@ -7,4 +14,20 @@ function prepareEditModal(trackID, trackName, personID, personName, character, k
     $("#edit-modal-kart-field").val(kart);
     $("#edit-modal-wheels-field").val(wheels);
     $("#edit-modal-glider-field").val(glider);
+
+    if (!allowsCustomisation)
+    {
+        $("#wheels-field").hide();
+        $("#glider-field").hide();
+
+        if (!choiceOfKart)
+        {
+            $("#kart-field").hide();
+        }
+    }
+}
+
+function submitTime()
+{
+
 }
