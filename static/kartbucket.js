7069dcc672e8a0f017ec6e374a6d097a89efefa3
Josh Asch <josh.asch@gmail.com>
2015-01-31 22:00:17 +0000

    Reset time fields each time and store selected IDs

--- static/kartbucket.js
+++ static/kartbucket.js
@@ -1,15 +1,18 @@
 
-var trackID = -1;
-var personID = -1;
-var character = "";
-var kart = "";
-var wheels = "";
-var glider = "";
+var newTrackID = -1;
+var newPersonID = -1;
 
 function prepareEditModal(trackID, trackName, personID, personName, character, kart, wheels, glider, choiceOfKart, allowsCustomisation)
 {
     $("#edit-modal .modal-title").html("Add Time - " + trackName);
+
     $("#edit-modal-person-field").val(personID);
+
+    $("#edit-modal-time-field").val("");
+    $("#edit-modal-split1-field").val("");
+    $("#edit-modal-split2-field").val("");
+    $("#edit-modal-split3-field").val("");
+
     $("#edit-modal-character-field").val(character);
     $("#edit-modal-kart-field").val(kart);
     $("#edit-modal-wheels-field").val(wheels);
@@ -25,6 +28,9 @@ function prepareEditModal(trackID, trackName, personID, personName, character, k
             $("#kart-field").hide();
         }
     }
+
+    newTrackID = trackID;
+    newPersonID = personID;
 }
 
 function submitTime()
