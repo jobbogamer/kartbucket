60c99517fca2686539f6f3b9e17382861efc8374
Josh Asch <josh.asch@gmail.com>
2015-02-01 11:25:45 +0000

    Allow entry of a URL for proof instead of upload

--- static/kartbucket.js
+++ static/kartbucket.js
@@ -2,12 +2,13 @@
 var newTrackID = -1;
 var newPersonID = -1;
 var newProofURL = "";
+var useFileUpload = true;
 
 function prepareEditModal(trackID, trackName, personID, personName, character, kart, wheels, glider, choiceOfKart, allowsCustomisation)
 {
     $("#edit-modal .modal-title").html("Submit Time - " + trackName);
 
-    $("#edit-modal-person-field").val(personID);
+    $("#edit-modal-person-field").html(personName);
 
     $("#edit-modal-time-field").val("");
     $("#edit-modal-split1-field").val("");
@@ -21,7 +22,7 @@ function prepareEditModal(trackID, trackName, personID, personName, character, k
 
     $("#edit-modal-proof-field").val(null);
     $("#proof-url").html("");
-    $("#upload-indicator").html('<i class="fa fa-fw fa-circle-o"></i>');
+    $("#upload-indicator").html('<a href="#" onclick="useURLForProof()"><i class="fa fa-fw fa-link"></i></a>');
 
     if (!allowsCustomisation)
     {
@@ -40,15 +41,24 @@ function prepareEditModal(trackID, trackName, personID, personName, character, k
 
 function submitTime()
 {
-    var time = $("#edit-modal-character-field").val();
-    var split1 = $("#edit-modal-character-field").val();
-    var split2 = $("#edit-modal-character-field").val();
-    var split3 = $("#edit-modal-character-field").val();
+    var time = $("#edit-modal-time-field").val();
+    var split1 = $("#edit-modal-split1-field").val();
+    var split2 = $("#edit-modal-split2-field").val();
+    var split3 = $("#edit-modal-split3-field").val();
 
     var character = $("#edit-modal-character-field").val();
     var kart = $("#edit-modal-kart-field").val();
     var wheels = $("#edit-modal-wheels-field").val();
     var glider = $("#edit-modal-glider-field").val();
+
+    if (useFileUpload)
+    {
+        var proof = newProofURL;
+    }
+    else
+    {
+        var proof = $("#edit-modal-url-field").val();
+    }
 
     $.ajax({
         url: '/api/submit',
@@ -63,20 +73,20 @@ function submitTime()
             'kart': kart,
             'wheels': wheels,
             'glider': glider,
-            'proof': newProofURL
+            'proof': proof
         }
     }).done(function(result) {
         if (result['success']) {
             
         } else {
-
+            console.log(result);
         }
     });
 }
 
 function uploadToImgur(file, form)
 {
-    $("#upload-indicator").html('<i class="fa fa-fw fa-spin fa-circle-o-notch"></i>');
+    $("#upload-indicator").html('<i class="fa fa-fw fa-spin fa-spinner"></i>');
     if (!file || !file.type.match(/image.*/)) return;
     var fd = new FormData();
     fd.append("image", file);
@@ -92,4 +102,18 @@ function uploadToImgur(file, form)
     }
     xhr.setRequestHeader('Authorization', 'Client-ID d8f7791810ee729');
     xhr.send(fd);
+}
+
+function useFileForProof()
+{
+    useFileUpload = true;
+    $("#url-field").slideUp();
+    $("#upload-field").slideDown();
+}
+
+function useURLForProof()
+{
+    useFileUpload = false;
+    $("#upload-field").slideUp();
+    $("#url-field").slideDown();
 }
