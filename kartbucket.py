2abeeb6be7137441cf78cc839f0e0c075f2c951d
Josh Asch <josh.asch@gmail.com>
2015-02-12 22:51:40 +0000

    Release v1.0.4

--- kartbucket.py
+++ kartbucket.py
@@ -20,7 +20,7 @@ except KeyError as error:
 database.db.init_app(app)
 
 options = {
-    'version': "1.0.3"
+    'version': "1.0.4"
 }
 
 # Horrible flask-ness to allow access to the database from outside of
