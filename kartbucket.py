e020c6adfe4fbeca3c9375c25a2a7ff75eb51def
Josh Asch <josh.asch@gmail.com>
2015-04-25 11:06:07 +0100

    Add MK×Animal Crossing tracks

--- kartbucket.py
+++ kartbucket.py
@@ -20,7 +20,7 @@ except KeyError as error:
 database.db.init_app(app)
 
 options = {
-    'version': "1.0.5"
+    'version': "1.0.6"
 }
 
 # Horrible flask-ness to allow access to the database from outside of
